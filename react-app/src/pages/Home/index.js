10ac4a69fce82c5dc1083eb181f19b486defcd40
gcheng720 <104961908+gcheng720@users.noreply.github.com>
2024-03-10 14:33:01 -0700

    Update index.js

--- react-app/src/pages/Home/index.js
+++ react-app/src/pages/Home/index.js
@@ -14,25 +14,87 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import React from "react";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography, Textfield } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Paper
-        elevation={3}
-        sx={{
-          width: "800px",
-          margin: "0 auto",
-          padding: (theme) => theme.spacing(3, 2),
-        }}
-      >
-        <Typography variant="h5">Welcome to the Fancy Store!</Typography>
-        <br />
-        <Typography variant="body1">
-          Take a look at our wide variety of products.
-        </Typography>
-      </Paper>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+        />
+        <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+      </div>
+      <div>
+        <TextField
+          id="filled-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="filled"
+        />
+        <TextField
+          id="filled-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+          variant="filled"
+        />
+        <TextField
+          id="filled-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+        <TextField
+          id="standard-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+          variant="standard"
+        />
+        <TextField
+          id="standard-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          variant="standard"
+        />
+      </div>
     </Box>
   );
 }
